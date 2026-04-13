d0870caaa3c25f6310cc0834dce38c80ea416396
simon.le-clerc <slc.le.clerc@gmail.com>
2026-04-14 00:07:04 +0200

    Add Tailwind CSS v4 via Vite plugin

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./ui/App";
 import { container } from "./app/container";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
